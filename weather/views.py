b5e33db3b239ceec59a78090fd60b31229bb5206
mana <manashams94@gmail.com>
2022-04-04 12:31:48 +0430

    LoginUserView added.

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from django.views.generic import CreateView
 from weather.forms import SignUpForm
+from django.contrib.auth.views import LoginView
 from .utils import get_data
 from dotenv import load_dotenv
 
@@ -16,6 +17,18 @@ class SignUpView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        for key, value in get_data('tehran').items():
+            context[key] = value
+        return context
+
+
+class LoginUserView(LoginView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['res'] = 'res'
         for key, value in get_data('tehran').items():
             context[key] = value
         return context
