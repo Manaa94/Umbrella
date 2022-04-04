0ea6403dbe7b477c989f0e78e5795ac188acd43b
mana <manashams94@gmail.com>
2022-04-04 12:30:38 +0430

    SignUpView added.

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView
+from weather.forms import SignUpForm
+from .utils import get_data
+from dotenv import load_dotenv
 
-# Create your views here.
+load_dotenv()
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = 'index.html'
+
+    def get_success_url(self):
+        return reverse('weather:login')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        for key, value in get_data('tehran').items():
+            context[key] = value
+        return context
